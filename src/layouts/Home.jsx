e2c2fc14784ae752037dda86ae9839aedc5a0185
BRHeaven <brheaven872@gmail.com>
2023-05-16 02:55:40 +0700

    update folder necessary

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import "../css/main.css";
+function Home(props) {
+  return (
+    <section>
+
+    </section>
+  );
+};
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    };
+};
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
